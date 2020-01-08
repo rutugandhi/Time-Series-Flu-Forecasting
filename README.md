d755ad412849b0267210448bbc1d090b4a2815f8
rutugandhi <rutugandhi21@gmail.com>
2020-01-08 11:00:41 +0530

    Update

--- README.md
+++ README.md
@@ -34,3 +34,20 @@ python LSTM.py --n_lag 4 --n_seq 4 --n_test 1 --n_epochs 1000 --n_batch 1 n_neur
 
 
 ```
+2. This step stores all the point forecasts to a file which is used by the points to bins converter in the next section.
+
+## Points to Bins
+1. Run the points to bins converter.
+```
+arguments with default values:
+sample_submission = "/submissions/WEEK-CEID-2019.csv"
+weeks_ahead = 1
+location = "HHS Region 1"
+
+```
+
+Example:
+```
+python pointsAndBins.py --sample_submission = /path/to/file --weeks_ahead 1 --location "HHS Region 1"
+
+```
--- pointsAndBins.py
+++ pointsAndBins.py
@@ -5,8 +5,19 @@ import argparse
 
 parser.add_argument('--sample_submission', type=str, default="/submissions/WEEK-CEID-2019.csv",
                     help="sample submission")
+
+parser.add_argument('--weeks_ahead', type=int, default=1, choices=[1, 2, 3, 4], help="target week")
+
+parser.add_argument('--location', type=str, default="HHS Region 1",
+                    choices=["US National", "HHS Region 1", "HHS Region 2", "HHS Region 3",
+                             "HHS Region 4", "HHS Region 5", "HHS Region 6", "HHS Region 7",
+                             "HHS Region 8", "HHS Region 9", "HHS Region 10"],
+                    help="location to forecast for")
+
 args = parser.parse_args()
 sample_submission = args.sample_submission
+weeks_ahead = weeks_ahead
+location = location
 
 # def four_week_ahead_bins(point, std_dev):
 num_std_dev = 2
@@ -32,4 +43,4 @@ bin_prob = point_and_bins(1.07543445, 2)
 import pandas as pd
 full = pd.read_csv(sample_submission)
 
-full[((full["Location"]=="HHS Region 1") & (full["Target"]=="4 wk ahead")) ]["Value"]=bin_prob
+full[((full["Location"]==location) & (full["Target"]==str(weeks_ahead) +" wk ahead")) ]["Value"]=bin_prob
